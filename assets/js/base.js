7ea449ceade60a08e2386d060f4a5dba67923953
zhoushijia <zhoushijia@foxmail.com>
2020-12-31 16:27:22 +0800

    基本资料js逻辑

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,8 +1,21 @@
 // 拦截器
 // 这里的形参options就是ajax请求中的实参对象
 $.ajaxPrefilter(function (options) {
+  // 每个请求都得拼接根路径
   options.url = 'http://ajax.frontend.itheima.net' + options.url
+  // 如果是需要令牌的就在请求前加上头部
   if (options.url.indexOf('/my') !== -1) {
     options.headers = { Authorization: localStorage.getItem('token') || '' }
+  }
+
+  // 请求完成
+  options.complete = function (res) {
+    // 服务器响应信息为不符合要求
+    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+      // 强制清除令牌
+      localStorage.removeItem('token')
+      // 跳转到登陆页面
+      location.href = '/login.html'
+    }
   }
 })
---
+++ assets/js/user/user_info.js
@@ -0,0 +1,45 @@
+$(function () {
+  const form = layui.form
+  const layer = layui.layer
+  //   自定义layui表单验证
+  form.verify({
+    nickname: function (value) {
+      if (value.length > 6) return '昵称长度为1-6位'
+    }
+  })
+
+  initUserMsg()
+  //从服务器获取信息
+  function initUserMsg() {
+    $.ajax({
+      type: 'GET',
+      url: '/my/userinfo',
+      success(res) {
+        if (res.status !== 0) return layer.msg('信息获取失败')
+        // 渲染  给所有表单元素赋值
+        form.val('formUserInfo', res.data)
+      }
+    })
+  }
+
+  //   重置 重新从服务器调数据
+  $('#btnReset').on('click', function (e) {
+    e.preventDefault()
+    initUserMsg()
+  })
+
+  //   修改后监听提交
+  $('.layui-form').on('submit', function (e) {
+    e.preventDefault()
+    $.ajax({
+      type: 'POST',
+      url: '/my/userinfo',
+      data: $(this).serialize(),
+      success(res) {
+        if (res.status !== 0) return layer.msg('提交失败')
+        // 修改父页面的头像和name信息
+        window.parent.getUserMsg()
+      }
+    })
+  })
+})
